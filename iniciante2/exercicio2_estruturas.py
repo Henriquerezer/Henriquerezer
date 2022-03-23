2c5cff1a5ef813aa07c4338fbf547e9e5869dd95
Henrique Rezer Mosquér <87787728+Henriquerezer@users.noreply.github.com>
2022-03-23 14:37:51 -0300

    Add files via upload

---
+++ iniciante2/exercicio2_estruturas.py
@@ -0,0 +1,8 @@
+## (acumulador) apresentar o total da soma obtida dos cem primeiros números inteiros
+print("Bem-vindo ao acumulador")
+acumulador = 1
+soma = 0
+while acumulador <= 100:
+    soma = soma + acumulador
+    print(soma)
+    acumulador += 1
