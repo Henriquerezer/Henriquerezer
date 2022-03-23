3dae694cfa257e22603f8eb7b635d5c3c3d98f50
Henrique Rezer Mosquér <87787728+Henriquerezer@users.noreply.github.com>
2022-03-23 14:33:32 -0300

    Add files via upload

---
+++ iniciante2/entradaUm.py
@@ -0,0 +1,16 @@
+print('digite o valor 1')
+valor1 = float(input())
+print('digite o valor 2')
+valor2 = float(input())
+
+print(valor1, valor2)
+
+soma = valor1 + valor2
+subtracao = valor1 - valor2
+multiplicacao = valor1 * valor2
+divisao = valor1 + valor2
+
+print('soma: ', soma)
+print('subtração: ', subtracao)
+print('Multipiplicação: ', multiplicacao)
+print('Divisão: ', divisao)
