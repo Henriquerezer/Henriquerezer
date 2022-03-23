d97416409cd4521cbea087ac35b95dc0c77b454b
Henrique Rezer Mosquér <87787728+Henriquerezer@users.noreply.github.com>
2022-03-23 14:37:25 -0300

    Add files via upload

---
+++ iniciante2/exemplo1_vetores.py
@@ -0,0 +1,13 @@
+import numpy
+
+vetor = numpy.empty(5)
+vetor[0] = 10
+vetor[1] = 20
+vetor[2] = 30
+vetor[3] = 40
+vetor[4] = 50
+
+##print(vetor[3])
+
+for posicao in range (0,5):
+    print(posicao)
