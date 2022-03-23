d06886191b360ac1ff3416e28c4ddbb4d6523d7d
Henrique Rezer Mosquér <87787728+Henriquerezer@users.noreply.github.com>
2022-03-23 14:34:34 -0300

    Add files via upload

---
+++ iniciante2/exercicio2.py
@@ -0,0 +1,10 @@
+print('Bem vindo a ferramenta de Calculo de volume de uma caixa retângular')
+print('Insira o valor de comprimento: ')
+comprimento = float(input())
+print('Insira o valor da Largura: ')
+largura = float(input())
+print('Insira o valor da altura: ')
+altura = float(input())
+volume = comprimento * largura * altura
+print('O volume da caixa retângular é: ',volume )
+
