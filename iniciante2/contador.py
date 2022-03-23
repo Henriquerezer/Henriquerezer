b0f3a508def3d182137bc956ae2e8b5626633fdf
Henrique Rezer Mosquér <87787728+Henriquerezer@users.noreply.github.com>
2022-03-23 14:36:02 -0300

    Add files via upload

---
+++ iniciante2/contador.py
@@ -0,0 +1,7 @@
+## apresentar o quadrado dos números inteiros de 15 a 200
+print("Bem-vindo ao contador")
+contador = 15
+while contador <= 200:
+    quadrado = contador * contador
+    print(quadrado)
+    contador += 1
