057e4b98d4598952b194e23f50b5d9df62597532
Henrique Rezer Mosquér <87787728+Henriquerezer@users.noreply.github.com>
2022-03-23 14:35:09 -0300

    Add files via upload

---
+++ iniciante2/exercicio3.py
@@ -0,0 +1,13 @@
+print('Bem-vindo a ferramenta de càlculo da área de um triãngulo ')
+
+
+print('Insira o valor da altura do triangulo: ')
+altura = float(input())
+
+print('Insira o valor da base: ')
+base = float(input())
+
+volume = (base * altura) / 2
+
+print('O valor do volume do triângulo é: ', volume)
+
